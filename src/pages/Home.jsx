c6fc2aeb93400115e15ef1ac52916c4b7e72e197
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-04-19 05:13:24 +0500

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,13 @@
-import React from 'react'
 
-const Home = (props) => {
-  const {users} = props
+import Form from "../components/Form"
+
+const Register = () => {
   return (
-    <div>
-    <div className="table-section pt-4 w-full ">
-    <table className="w-1/2 mx-auto bg-[#191122] text-white border align-middle">
-      <thead>
-        <tr>
-          <th className="border">ID</th>
-          <th className="border">Name</th>
-          <th className="border">Email</th>
-          <th className="border">Age</th>
-          <th className="border">Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          users.length > 0 && users.map((user, index) =>{
-            return (
-              <tr key={index}>
-          <td className="border text-center">{index + 1}</td>
-          <td className="border text-center">{user.name}</td>
-          <td className="border text-center">{user.email}</td>
-          <td className="border text-center">{user.age}</td>
-          <td className="border text-center">
-            <span className="flex gap-4 justify-center">
-              <FaEdit className="cursor-pointer text-blue-600" />
-              <FaTrashAlt onClick={() => {handleDelete(user.id)}} className="cursor-pointer  text-red-600 " />
-            </span>
-          </td>
-        </tr>
-            )
-          })
-        }
-      </tbody>
-    </table>
-  </div>
-</div>
-
+    <div className='w-[500px] bg-white rounded-lg mx-auto h-[580px] mt-4 py-6 px-[40px]'>
+      <h1 className="text-center font-bold text-2xl mb-8">Let's go</h1> 
+      <Form></Form>
+    </div>
   )
 }
 
-export default Home
-
+export default Register
